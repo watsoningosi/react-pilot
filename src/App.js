5f9182704f9ea1a53c88f76e3b1d393e5a42d725
watsoningosi <watstarsparta@gmail.com>
2022-10-15 12:41:16 +0300

    Todo Canvas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState} from 'react';
+import TodoList from './TodoList';
 
 function App() {
+  const [todos, setTodos] = useState(['Todo1','Todo2'])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <TodoList todos={todos} />
+      <input type="text" /> 
+      <button> Add Todo</button>
+      <button> Clear Done Todo</button>
+      <div>0 left to do</div>
+      </>
+  )
 }
 
 export default App;
+ 
