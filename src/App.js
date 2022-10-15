75518ee89c6855d95d78b9b15cf04fd2503696d0
watsoningosi <watstarsparta@gmail.com>
2022-10-15 14:09:49 +0300

    pending local storage commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React,{ useState} from 'react';
+import React,{ useState, useRef, useEffect} from 'react';
 import TodoList from './TodoList';
+import uuid from 'react-uuid';
+ 
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
-  const [todos, setTodos] = useState(['Todo1','Todo2'])
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef();
+
+  useEffect(() => {
+
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify)
+
+  },[todos] )
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(PrevTodos => {
+      return [...PrevTodos, { id: uuid, name: name, complete: false }]
+      
+    });
+    todoNameRef.current.value = null
+  }
   return (
     <>
       <TodoList todos={todos} />
-      <input type="text" /> 
-      <button> Add Todo</button>
+      <input ref={todoNameRef} type="text" /> 
+      <button onClick={handleAddTodo}> Add Todo</button>
       <button> Clear Done Todo</button>
       <div>0 left to do</div>
       </>
--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-export default function Todo({todo}) {
-  return (
+export default function Todo({ todo, toggleTodo }) {
+    function handleTodoClick() {
+      toggleTodo(todo.id)
+    }
+    
+    return (
       <div>
-          {todo}
-    </div>
-  )
-}
+        <label>
+          <input type="checkbox" checked={todo.complete} onChange={handleTodoClick} />
+          {todo.name}
+        </label>
+      </div>
+    )
+  }
